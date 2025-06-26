b2edb0d0e9f2100f451fd3a2b861ce560261c096
Aditya Singh <114923355+Aditya0320@users.noreply.github.com>
2025-06-26 16:52:44 +0000

    Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert(3 + 3) we are using nodejs, not browser 
+
+console.log(3 + 3) 
+
+console.log("Aditya")
+
+/* there is two documentation for javascript 
+ 1. tc39.es
+ 2. MDN (developer.mozilla.org) */
+
+ let name = "Aditya" // string data type 
+ let age = 21 // number 
+ let isLoggedIn = false // boolean 
+ let state;  // value is not assigned 
+
+ // number  => 2 to power 53
+ // bigint 
+ // string => ""
+ // boolean => true/false
+ // null => standalone value 
+ // undefined => 
+ // symbol => unique 
+ 
+ 
+
+ // object
+
+ console.log(typeof undefined); // undefined 
+ console.log(typeof null); // object 
+ 
